c40bc8782b2180d48d14990f948b47c4f6e7ac2b
nurulabsr <absar.ru@yandex.com>
2023-12-14 21:38:36 +0600

    Updated

---
+++ resources/views/index.blade.php
@@ -0,0 +1,48 @@
+@extends('main.main')
+
+@section('content')
+
+<div class="main-content mt-5">
+    <div class="card">
+        <div class="card-header">
+            All post
+            <a href="" class="btn btn-secondary">Create</a>
+            <a href="" class="btn btn-warning">Recycle Bin</a>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Image</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Category</th>
+                            <th scope="col">Publish Date</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>
+                                <img src="https://picsum.photos/200" alt="" class="img-fluid" width="80">
+                            </td>
+                            <td>Lorem ipsum dolor sit amet</td>
+                            <td>Lorem ipsum dolor sit amet consectetur adipisicing el. Lorem ipsum dolor sit amet.</td>
+                            <td>Hunter</td>
+                            <td>12/14/2023</td>
+                            <td>
+                                <a href="" class="btn btn-primary btn-sm">Edit</a>
+                                <a href="" class="btn btn-danger btn-sm">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
