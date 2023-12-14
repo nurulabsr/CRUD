d7914bf36bdf92f565d397fc3934f797cb7170ab
nurulabsr <absar.ru@yandex.com>
2023-12-14 22:27:03 +0600

    Category option and Button added

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -10,8 +10,8 @@
               <h4>Create post</h4>
             </div>
             <div class="col-md-6 d-flex justify-content-end">
-                <a href="" class="btn btn-secondary">Create</a>
-                <a href="" class="btn btn-warning">Recycle Bin</a>
+                <a href="" class="btn btn-secondary m-1">Back</a>
+                <a href="" class="btn btn-warning m-1">Recycle Bin</a>
             </div>
            </div>
         </div>
@@ -27,11 +27,23 @@
                 <input type="text" class="form-control">
             </div>
             <div class="form-group">
+                <label class="form-label" for="">Category</label>
+                <select class="form-control" name="" id="">
+                    <option value="">Assembly Language</option>
+                    <option value="">Rust Language</option>
+                    <option value="">C++ Language</option>
+                    <option value="">Java Language</option>
+                </select>
+
+            </div>
+            <div class="form-group">
                 <label class="form-label" for="">Description</label>
                 <textarea class="form-control" name="" id="" cols="30" rows="10"></textarea>
             </div>
 
-            
+            <div class="form-group mt-3">
+                <button class="btn btn-primary">Submit</button>
+            </div>
            </form>
         </div>
     </div>
