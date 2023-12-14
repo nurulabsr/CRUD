9d80f64032a1ed216fe7e20966010b689f6fec1f
nurulabsr <absar.ru@yandex.com>
2023-12-14 21:59:14 +0600

    create.blade.php added

---
+++ resources/views/create.blade.php
@@ -0,0 +1,24 @@
+@extends('main.main')
+
+@section('content')
+
+<div class="main-content mt-5">
+    <div class="card">
+        <div class="card-header">
+           <div class="row">
+            <div class="col-md-6">
+              <h4>All post</h4>
+            </div>
+            <div class="col-md-6 d-flex justify-content-end">
+                <a href="" class="btn btn-secondary">Create</a>
+                <a href="" class="btn btn-warning">Recycle Bin</a>
+            </div>
+           </div>
+        </div>
+        <div class="card-body">
+           
+        </div>
+    </div>
+</div>
+
+@endsection
